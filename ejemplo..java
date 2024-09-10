7e097b0eaeddee1410f99ade621464c2485d281d
xleoh <124121327@morelia.tecnm.mx>
2024-09-10 13:29:05 -0600

    java

---
+++ ejemplo..java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Andres Leonardo Huerta Hurtado";
+        int edad = 21;
+        char genero = 'h';
+        double estatura = 1.84;
+        boolean esCasado = false;
+
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(genero);
+        System.out.println(estatura);
+        System.out.println(esCasado);
+        System.out.println("mi nombre es:"+nombre);
+        System.out.println("mi edad es:"+edad);
+        System.out.println("mi genero es:"+genero);
+        System.out.println("mi estatura es:"+estatura);
+        System.out.println("mi esCasado es:"+esCasado);
+
+        String  parentezco = "Madre";
+        String suNombre = "Elena Hurtado Torres";
+        char Genero = 'M';
+        double Estatura = 1.61;
+        boolean esCasada = false;
+        int añoDeNacimiento = 1974;
+
+        System.out.println(parentezco);
+        System.out.println(suNombre);
+        System.out.println(Genero);
+        System.out.println(Estatura);
+        System.out.println(esCasada);
+        System.out.println(añoDeNacimiento);
+
+    }
+}
